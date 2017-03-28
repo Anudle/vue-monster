ff5386787a3a8c2463d8492c5d3d83f0d62b52f3
Anudle <AnuShrestha007@gmail.com>
2017-03-28 15:37:00 -0600

    getting along

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
 new Vue({
-  el: '#app',
-  data: {
-    options: false,
-    humanHealth: {
-      width: '100',
-      backgroundColor: 'green',
-      margin: '0',
-      color: 'white',
+    el: '#app',
+    data: {
+        gameisRunning: false,
+        humanHealth: 100,
+        monsterHealth: 100,
+
+
     },
-    monsterHealth: {
-      width: '100',
-      backgroundColor: 'green',
-      margin: '0',
-      color: 'white',
+    methods: {
+        startGame: function() {
+            this.gameisRunning = true;
+            this.humanHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function() {
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * 10) + 1, min)
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <= 0){
+              alert('You won!');
+              this.gameisRunning = false;
+            }
+            max = 11
+            min = 4
+            var damage = Math.max(Math.floor(Math.random() * 10) + 1, min)
+            this.humanHealth -= damage
+            if(this.humanHealth <= 0){
+              alert('You lost!');
+              this.gameisRunning = false;
+            }
+        },
+        specialAttack: function() {
+            var vm = this;
+            var humanSpecialAttack = Math.floor(Math.random() * 20) + 1;
+            var monsterSpecialAttack = Math.floor(Math.random() * 20) + 1
+            console.log('humman specialAttack ' + humanSpecialAttack);
+            console.log('monster specialAttack ' + monsterSpecialAttack);
+            vm.humanHealth = vm.humanHealth - humanSpecialAttack;
+            vm.monsterHealth = vm.monsterHealth - monsterSpecialAttack
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
+        }
+    },
+    computed: {
+
     }
 
-  },
-  methods: {
-    attack: function(){
-    var vm = this
-    var humanAttack = Math.floor(Math.random() * 10) + 1
-    var monsterAttack = Math.floor(Math.random() * 10) + 1
-    console.log('human attack is ' + humanAttack)
-    console.log('monster attack is ' + monsterAttack)
-    var updateHumanHealth = vm.humanHealth.width - humanAttack;
-    vm.humanHealth.width = updateHumanHealth + '%';
-    }
-  }
+
 })
